fdca57a64230189d72a96da6263134d1505c458c
Jeremy <mail@jez500.com>
2025-02-11 14:43:12 +1100

    Added search to docs

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -2,40 +2,44 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { googleTagManagerPlugin } from '@vuepress/plugin-google-tag-manager'
+import { searchPlugin } from '@vuepress/plugin-search'
 
 export default defineUserConfig({
-  lang: 'en-US',
+    lang: 'en-US',
 
-  title: 'PriceBuddy Documentation',
-  description: 'A dollar saved is a dollar earned',
+    title: 'PriceBuddy Documentation',
+    description: 'A dollar saved is a dollar earned',
 
-  theme: defaultTheme({
-      logo: '/logo-light.svg',
-      logoDark: '/logo-dark.svg',
+    theme: defaultTheme({
+        logo: '/logo-light.svg',
+        logoDark: '/logo-dark.svg',
 
-      navbar: [
-          {link: '/installation', text: 'Docs'},
-          {link: '/features', text: 'Features'},
-          {link: 'https://github.com/jez500/pricebuddy', text: 'Github'},
-      ],
+        navbar: [
+            {link: '/installation', text: 'Docs'},
+            {link: '/features', text: 'Features'},
+            {link: 'https://github.com/jez500/pricebuddy', text: 'Github'},
+        ],
 
-      sidebar: [
-          '/',
-          '/installation',
-          '/stores',
-          '/products',
-          '/tags',
-          '/settings',
-          '/users',
-          '/log-messages',
-      ],
-  }),
-
-  bundler: viteBundler(),
-
-  plugins: [
-    googleTagManagerPlugin({
-        id: 'GTM-KNMBZRWX',
+        sidebar: [
+            '/',
+            '/installation',
+            '/stores',
+            '/products',
+            '/tags',
+            '/settings',
+            '/users',
+            '/log-messages',
+        ],
     }),
-  ],
+
+    bundler: viteBundler(),
+
+    plugins: [
+        googleTagManagerPlugin({
+            id: 'GTM-KNMBZRWX',
+        }),
+        searchPlugin({
+            //
+        })
+    ],
 })
