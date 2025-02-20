c29f8f361fd5a1bd27a1063bd1e10774ad023730
Jeremy <mail@jez500.com>
2025-02-20 15:02:59 +1100

    Disabled breadcrumbs

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -77,6 +77,7 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->breadcrumbs(false)
             ->databaseNotifications();
     }
 }
