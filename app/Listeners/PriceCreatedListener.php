782ae2748446dc0e16d3f08114ab3ce948fa6c14
Jeremy <mail@jez500.com>
2025-02-26 13:36:49 +1100

    feat: catch errors when notification fails and send to log

--- app/Listeners/PriceCreatedListener.php
+++ app/Listeners/PriceCreatedListener.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use App\Events\PriceCreatedEvent;
 use App\Notifications\PriceAlertNotification;
+use Exception;
 
 class PriceCreatedListener
 {
@@ -21,9 +22,19 @@ class PriceCreatedListener
             return;
         }
 
-        // Notify the user if the price is within the range.
-        if ($product->shouldNotifyOnPrice($event->price->price)) {
-            $product->user?->notify(new PriceAlertNotification($url));
+        try {
+            // Notify the user if the price is within the range.
+            if ($product->shouldNotifyOnPrice($event->price->price)) {
+                $product->user?->notify(new PriceAlertNotification($url));
+            }
+        } catch (Exception $e) {
+            // Log the error.
+            logger()->error('Error sending price alert notification: '.$e->getMessage(), [
+                'product' => $product->title,
+                'product_id' => $product->getKey(),
+                'url' => $event->price->url,
+                'url_id' => $event->price->getKey(),
+            ]);
         }
     }
 }
